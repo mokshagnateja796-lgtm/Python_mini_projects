31091c7ab7e4e3a43e4867e47d63966838b6673a
Mokshagna Teja <mokshagnateja796@gmail.com>
2026-07-15 15:37:21 +0530

    First commit

---
+++ ATM_project/atm.py
@@ -0,0 +1,36 @@
+balance = 5000
+
+while True:
+    print("ATM Menu")
+    print("1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+
+    choice = int(input("Enter the Choice: "))
+
+    if choice == 1:
+        print("Current Balance:", balance)
+
+    elif choice == 2:
+        money = int(input("Enter the money: "))
+        balance = balance + money
+        print("Your money is Deposited")
+        print("Current Balance:", balance)
+
+    elif choice == 3:
+        money = int(input("Enter the money: "))
+
+        if money <= balance:
+            balance = balance - money
+            print("Your amount Withdraw done")
+            print("Current Balance:", balance)
+        else:
+            print("Insufficient amount")
+
+    elif choice == 4:
+        print("Thank You")
+        break
+
+else:
+ print("Invalid Choice")
